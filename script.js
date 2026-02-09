59ec4bbbfc5d3e566776f52ba3c642edef3eb82f
dedisson-pro <juliotcarlossoho@gmail.com>
2026-02-09 00:38:17 +0000

    Add function to display a message in HTML

---
+++ script.js
@@ -0,0 +1,4 @@
+function afficherMessage() {
+    document.getElementById("message").innerHTML =
+        "J'apprends chaque jour à devenir développeur web et à créer des solutions utiles.";
+                            }
